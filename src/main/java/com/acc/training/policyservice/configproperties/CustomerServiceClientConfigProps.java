eff1c112df3f53fcb478c8ffa3c425e4ae748b84
lnrajput <lnrajput@gmail.com>
2021-01-05 12:55:02 -0500

    updated profile info

--- src/main/java/com/acc/training/policyservice/configproperties/CustomerServiceClientConfigProps.java
+++ src/main/java/com/acc/training/policyservice/configproperties/CustomerServiceClientConfigProps.java
@@ -1,11 +1,9 @@
 package com.acc.training.policyservice.configproperties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("local")
 @ConfigurationProperties("external.services.customer")
 public class CustomerServiceClientConfigProps {
 
